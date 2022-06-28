5e445ef75f4e93b6f667515ceea9821c84b47cef
HarryET <29015545+HarryET@users.noreply.github.com>
2022-06-28 17:17:32 +0100

    Remove from sub-module
    In response to @ffrizzo's feedback I have gotten rid of the networking module and moved the contents into the `main.tf` file.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,40 @@ provider "aws" {
   region = var.region
 }
 
-module "networking" {
-  source = "./networking"
-
+locals {
   environment = terraform.workspace
   group       = "cloud_siwe_${terraform.workspace}"
+}
+
+resource "aws_vpc" "cloud_siwe_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name        = "cloud_siwe_${locals.environment}_vpc"
+    Environment = locals.environment # To make filtering easier
+    Group       = locals.group # To make filtering easier
+  }
+}
+
+resource "aws_subnet" "cloud_siwe_private_subnet" {
+  vpc_id                  = aws_vpc.cloud_siwe_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "cloud_siwe_${locals.environment}_private_subnet"
+    Environment = locals.environment # To make filtering easier
+    Group       = locals.group # To make filtering easier
+  }
+}
+
+resource "aws_subnet" "cloud_siwe_public_subnet" {
+  vpc_id     = aws_vpc.cloud_siwe_vpc.id
+  cidr_block = "10.0.2.0/24"
+  
+  tags = {
+    Name        = "cloud_siwe_${locals.environment}_public_subnet"
+    Environment = locals.environment # To make filtering easier
+    Group       = locals.group # To make filtering easier
+  }
 }
--- terraform/networking/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "group" {
-  type = string
-  default = "cloud_siwe"
-}
-
-variable "environment" {
-  type = string
-  default = "dev"
-}
