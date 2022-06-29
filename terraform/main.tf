78e51b548ec35818408fae3e559f1b8ba1083a74
HarryET <29015545+HarryET@users.noreply.github.com>
2022-06-29 17:09:40 +0100

    fix(ci): GitHub module wrong

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@ locals {
   group = "cloud-siwe-${terraform.workspace}"
 }
 
-module "base_tags" {
-  source = "WalletConnect/terraform-modules/tags"
+module "tags" {
+  source = "github.com/WalletConnect/terraform-modules/modules/tags"
 
   application = local.group
   env         = terraform.workspace
@@ -19,7 +19,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-  tags = module.base_tags.tags.value
+  tags = module.tags.tags
   private_subnet_tags = {
     Visibility = "private"
   }
