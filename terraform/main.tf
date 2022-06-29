da431de619d3159102c6e12100b3bdbf51d9ebb1
HarryET <29015545+HarryET@users.noreply.github.com>
2022-06-29 16:31:51 +0100

    Move provider to different file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,3 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = var.region
-
-  skip_get_ec2_platforms      = true
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-}
-
 locals {
   group = "cloud-siwe-${terraform.workspace}"
 }
---
+++ terraform/provider.tf
@@ -0,0 +1,10 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
