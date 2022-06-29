33abbb6504f454dbf801022a542aed67b5a7fe7c
Harry Bairstow <h.bairstow22@gmail.com>
2022-06-29 16:35:26 +0100

    feat: VPCs and Subnets (#1)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,29 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = var.region
+locals {
+  group = "cloud-siwe-${terraform.workspace}"
 }
 
+module "base_tags" {
+  source = "WalletConnect/terraform-modules/tags"
+
+  application = local.group
+  env         = terraform.workspace
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${local.group}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = var.azs
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  tags = module.base_tags.tags.value
+  private_subnet_tags = {
+    Visibility = "private"
+  }
+  public_subnet_tags = {
+    Visibility = "public"
+  }
+}
--- terraform/vars/dev.tfvars
+++ terraform/vars/dev.tfvars
@@ -1,2 +1,3 @@
 region = "eu-central-1"
+azs    = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 fqdn   = "dev.login.walletconnect.com"
