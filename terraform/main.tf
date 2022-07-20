00f2d3850ac5d4f93dea7396da3a833c47afe48f
Harry Bairstow <h.bairstow22@gmail.com>
2022-07-21 01:55:28 +0200

    more work

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,7 +75,7 @@ module "ecs" {
   gotrue_image_tag      = "0.1.6"
 
   proxy_repository_url = data.aws_ecr_repository.gotrue_proxy.repository_url
-  proxy_image_tag      = "0.1.17"
+  proxy_image_tag      = "0.1.24"
 
   cpu    = var.cpu
   memory = var.memory
